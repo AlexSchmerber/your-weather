4e3e5050e26296e338d4574fc584b4b7176f7e01
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-01-24 10:17:10 -0600

    search bar responsive to map and weather

--- weather_map.js
+++ weather_map.js
@@ -34,84 +34,50 @@ function reverseGeocode(coordinates, token) {
         });
 }
 
-const coordinatesGeocoder = function (query) {
-
-    const matches = query.match(
-        /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
-    );
-
-    if (!matches) {
-        return null;
-    }
-
-    function coordinateFeature(lng, lat) {
-        return {
-            center: [lng, lat],
-            geometry: {
-                type: 'Point',
-                coordinates: [lng, lat]
-            },
-            place_name: 'Lat: ' + lat + ' Lng: ' + lng,
-            place_type: ['coordinate'],
-            properties: {},
-            type: 'Feature'
-        };
-    }
-
-    const coord1 = Number(matches[1]);
-    const coord2 = Number(matches[2]);
-    const geocodes = [];
-
-    if (coord1 < -90 || coord1 > 90) {
-        // must be lng, lat
-        geocodes.push(coordinateFeature(coord1, coord2));
-    }
-
-    if (coord2 < -90 || coord2 > 90) {
-        // must be lat, lng
-        geocodes.push(coordinateFeature(coord2, coord1));
-    }
-
-    if (geocodes.length === 0) {
-        // else could be either lng, lat or lat, lng
-        geocodes.push(coordinateFeature(coord1, coord2));
-        geocodes.push(coordinateFeature(coord2, coord1));
-    }
-
-    return geocodes;
-
+function searchLocation() {
+    let searchInput = $('#search_bar')
+    let input = searchInput.focus().val()
+    console.log(input)
+    geocode(input, MAPBOX_API_KEY).then(function (result){
+        $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${result[1]}&lon=${result[0]}&appid=${OPENWEATHER_API_KEY}`, {
+            units: "imperial"
+        }).done(function(data) {
+            if(isNaN(input) === true){
+                map.setCenter(result)
+                const marker = new mapboxgl
+                    .Marker();
+                marker.setLngLat(result);
+                marker.addTo(map);
+                $('#forecast_banner').html(`<h1>${data.city.name}</h1>`)
+                for (let i = 0; i < 40; i ++) {
+                    if(i % 8 === 0 || i === 0){
+                        $('#forecast_banner').append(`<div class="bg-dark text-white pt-3 mx-2" style="display: inline-block; height: 100px; width: 250px;">${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br>${data.list[i].weather[0].description}</div>`);
+                    }
+                }
+            } else {
+                console.log('invalid input');
+            }
+        });
+    });
 };
 
-map.addControl(
-    new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        localGeocoder: coordinatesGeocoder,
-        zoom: 4,
-        placeholder: 'Search',
-        mapboxgl: mapboxgl,
-        reverseGeocode: true
-    })
-);
-
-let lattitude = '';
-let longitude = '';
+$('#search_bar').keypress(function (e) {
+    if (e.key === 'Enter'){
+        searchLocation();
+    }
+});
 
 
-
-
-
-geocode('San Antonio', MAPBOX_API_KEY).then(function (result){
+geocode('san antonio', MAPBOX_API_KEY).then(function (result){
     $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${result[1]}&lon=${result[0]}&appid=${OPENWEATHER_API_KEY}`, {
         units: "imperial"
     }).done(function(data) {
         $('#forecast_banner').html(`<h1>${data.city.name}</h1>`)
-        for (let i = 0; i <= 40; i ++) {
+        for (let i = 0; i < 40; i++) {
             if(i % 8 === 0 || i === 0){
-                $('#forecast_banner').append(`<div class="bg-dark text-white pt-3 mx-2" style="display: inline-block; height: 100px; width: 250px;">${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br> ${data.list[i].weather[0].description}</div></div>`);
+                $('#forecast_banner').append(`<div class="bg-dark text-white pt-3 mx-2" style="display: inline-block; height: 100px; width: 250px;">${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br> ${data.list[i].weather[0].description}</div>`);
             }
-
         }
-
     });
 });
 
@@ -127,8 +93,8 @@ function pinThatAddress(address) {
         .then(function (result) {
             const marker = new mapboxgl
                 .Marker();
-            lattitude = result[1];
-            longitude = result[0];
+            let lattitude = result[1];
+            let longitude = result[0];
 
             marker.setLngLat(result);
             marker.addTo(map);
