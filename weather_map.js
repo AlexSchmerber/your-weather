925685108ec985c4990ecccbcc6271adaa124483
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-01-24 12:53:18 -0600

    added banner style

--- weather_map.js
+++ weather_map.js
@@ -34,6 +34,10 @@ function reverseGeocode(coordinates, token) {
         });
 }
 
+
+let rainy = "img/rainy_clouds.jpeg"
+let sunny = "img/mostly_sunny.jpeg"
+let cloudy = "img/partly_cloudy.jpeg"
 function searchLocation() {
     let searchInput = $('#search_bar')
     let input = searchInput.focus().val()
@@ -46,22 +50,26 @@ function searchLocation() {
                 // if(data.list[0].weather[0].main === 'Rain'){
                 //     $('#container').css('background-image', 'url("img/rainy_clouds.jpeg")')
                 // }
-                let rainy = "img/rainy_clouds.jpeg"
-                let sunny = `url("img/mostly_sunny.jpeg")`
-                let cloudy = `url("img/partly_cloudy.jpeg")`
-//left here
-                $('body').css('background-image', `url('${rainy}')`)
+                if(data.list[0].weather[0].main === 'Rain'){
+                    $('body').css('background-image', `url('${rainy}')`)
+                }
+                if(data.list[0].weather[0].main === 'Clear'){
+                    $('body').css('background-image', `url('${sunny}')`)
+                }
+                if(data.list[0].weather[0].main === 'Clouds'){
+                    $('body').css('background-image', `url('${cloudy}')`)
+                }
                 map.setCenter(result)
                 const marker = new mapboxgl
                     .Marker();
                 marker.setLngLat(result);
                 marker.addTo(map);
                 console.log(data.list);
-                $('#forecast_banner').html(`<h1>${data.city.name}</h1><h3>Current ${data.list[0].main.temp}</h3>`)
+                $('#forecast_banner').html(`<div style="color: white;" ><h3>${data.city.name}</h3><h5>Current ${data.list[0].main.temp}</h5></div>>`)
                 for (let i = 0; i <= 32; i ++) {
                     if(i % 8 === 0 || i === 0){
                         let date = new Date(data.list[i].dt_txt);
-                        $('#forecast_banner').append(`<div class="bg-dark text-white pt-3 mx-2" style="display: inline-block; height: 100px; width: 250px;">${date.toDateString().substring(0, 7)} ${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br>${data.list[i].weather[0].description}</div>`);
+                        $('#forecast_banner').append(`<div class="bg-black text-white pt-3 mx-2" style="font-size: 12px; display: inline-block; height: 80px; width: 160px;">${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br> ${data.list[i].weather[0].description}</div>`);
                     }
                 }
             } else {
@@ -83,10 +91,10 @@ geocode('san antonio', MAPBOX_API_KEY).then(function (result){
         units: "imperial"
     }).done(function(data) {
         console.log(data);
-        $('#forecast_banner').html(`<h1>${data.city.name}<h3>Current ${data.list[0].main.temp}</h3></h1>`)
+        $('#forecast_banner').html(`<div style="color: white;" ><h3>${data.city.name}</h3><h5>Current ${data.list[0].main.temp}</h5></div>>`)
         for (let i = 0; i < 40; i++) {
             if(i % 8 === 0 || i === 0){
-                $('#forecast_banner').append(`<div class="bg-dark text-white pt-3 mx-2" style="display: inline-block; height: 100px; width: 250px;">${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br> ${data.list[i].weather[0].description}</div>`);
+                $('#forecast_banner').append(`<div class="bg-black text-white pt-3 mx-2" style="font-size: 12px; display: inline-block; height: 80px; width: 160px;">${data.list[i].dt_txt.substring(6,10)}<br>${data.list[i].main.temp} F<br> ${data.list[i].weather[0].description}</div>`);
             }
         }
     });
